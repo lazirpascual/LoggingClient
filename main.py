e1748abdaccc700f93424784a3037c6e713b772a
rohullahN <41601768+rohullahN@users.noreply.github.com>
2022-02-23 18:52:48 -0500

    testing

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ PORT = 13000  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b"?request=TEST")
+    s.sendall(b"?request=TEST1")
     data = s.recv(1024)
 
 print(f"Received {data!r}")
