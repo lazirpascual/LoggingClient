44ac8d0294b43a5c970aa815bc2cafb2d6df7ae7
rohullahN <41601768+rohullahN@users.noreply.github.com>
2022-02-23 21:23:26 -0500

    added functionality to choose log level

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # echo-client.py
 
 import socket
+from datetime import datetime
 
-HOST = "10.169.92.182"  # The server's hostname or IP address
+HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 13000  # The port used by the server
 
 
@@ -15,6 +16,38 @@ def socketConnection():
 
 
 choice = True
+timeStamp = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+errorLevel = ""
+
+def createLog():
+    print("""
+        1. INFO
+        2. WARNING
+        3. ERROR
+        4. FATAL
+        """)
+    level = input("Please select your log level ")
+
+    if level == "1":
+        errorLevel = " INFO"
+        # print(errorLevel)
+    elif level == "2":
+        errorLevel = " WARNING"
+        # print(errorLevel)
+    elif level == "3":
+        errorLevel = " ERROR"
+        # print(errorLevel)
+    elif level == "4":
+        errorLevel = " FATAL"
+        # print(errorLevel)
+    else:
+        errorLevel = ""
+        print("ERROR: Invalid selection")
+
+    message = input("Enter log details\n")
+    logMessage = timeStamp + errorLevel + " " + message
+    print(logMessage)
+
 while choice:
     print("""
     1. Test Logging Service
@@ -25,7 +58,7 @@ while choice:
     """)
     choice = input("What would you like to do? ")
     if choice == "1":
-        socketConnection()
+        createLog()
     elif choice == "2":
         print("\n ")
     elif choice == "3":
@@ -37,3 +70,5 @@ while choice:
         choice = False
     elif choice != "":
         print("\n Not Valid Choice Try again")
+
+
