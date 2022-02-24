63b8f146b7c517bd0cbc9ef25c77dc62a1fccb0b
Lazir Pascual <lpascual4579@conestogac.on.ca>
2022-02-23 19:22:20 -0500

    remove pycharm files

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import socket
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
+HOST = "10.169.92.37"  # The server's hostname or IP address
 PORT = 13000  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
