b2aa211e5ec3bcbd4e8b6309c36a1e0b3aa76de6
Lazir Pascual <lpascual4579@conestogac.on.ca>
2022-02-23 22:03:39 -0500

    send necessary data in a query string

--- main.py
+++ main.py
@@ -7,17 +7,18 @@ HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 13000  # The port used by the server
 
 
-def socketConnection():
+def socketConnection(queryString):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        s.sendall(b"?request=TEST1")
+        s.sendall(queryString)
         data = s.recv(1024)
         print(f"Received {data!r}")
 
 
 choice = True
-timeStamp = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+timeStamp = datetime.now().strftime("%m/%d/%Y,%H:%M:%S")
 errorLevel = ""
+
 
 def createLog():
     print("""
@@ -29,24 +30,25 @@ def createLog():
     level = input("Please select your log level ")
 
     if level == "1":
-        errorLevel = " INFO"
+        errorLevel = "INFO"
         # print(errorLevel)
     elif level == "2":
-        errorLevel = " WARNING"
+        errorLevel = "WARNING"
         # print(errorLevel)
     elif level == "3":
-        errorLevel = " ERROR"
+        errorLevel = "ERROR"
         # print(errorLevel)
     elif level == "4":
-        errorLevel = " FATAL"
+        errorLevel = "FATAL"
         # print(errorLevel)
     else:
         errorLevel = ""
         print("ERROR: Invalid selection")
 
     message = input("Enter log details\n")
-    logMessage = timeStamp + errorLevel + " " + message
-    print(logMessage)
+    query = f"?request=LOGTEST&timeStamp={timeStamp}&errorLevel={errorLevel}&message={message}"
+    socketConnection(bytes(query, "utf-8"))
+
 
 while choice:
     print("""
@@ -70,5 +72,3 @@ while choice:
         choice = False
     elif choice != "":
         print("\n Not Valid Choice Try again")
-
-
